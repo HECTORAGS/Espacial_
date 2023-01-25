b2aae620dee494a5b394713a4d5de318f22e4fbc
HECTORAGS <hector.delatorre@cimat.mx>
2023-01-25 17:14:10 -0600

    Nuevos cambios

--- intento1.R
+++ intento1.R
@@ -2,10 +2,12 @@ install.packages("rgdal")
 library(rgdal)
 library(dplyr)
 setwd("D:\\Users\\Hector de la Torre\\Documents\\Mapas\\")
-datos_jal<-readOGR("14_jalisco\\conjunto_de_datos",layer="14a",encoding = "ISO-8859-1" )
-plot(datos_jal)
-#dplyr::filter(datos_jal,CVE_MUN="074")
-datos_jal@data
-plot(datos_jal[which(datos_jal$CVE_MUN=="076"),])
-plot(datos_jal[which(datos_jal$CVE_MUN=="074"),])
+datos_ags<-readOGR("01_aguascalientes\\conjunto_de_datos",layer="01a",encoding = "ISO-8859-1" )
+plot(datos_ags)
+plot(datos_jal[which(datos_ags$CVE_MUN=="001" & datos_ags$CVE_LOC=="0001"),])
 
+head(datos_ags@data)
+table(datos_ags@data$CVE_MUN,datos_ags@data$CVE_LOC)
+
+#Leer info del Censo de Población y Vivienda 2022
+read.csv("P:\\MMOP\\Git\\RESAGEBURB_02CSV20.csv")
